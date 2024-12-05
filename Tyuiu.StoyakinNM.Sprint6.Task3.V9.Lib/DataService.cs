5b1e2c54b2dac7afd380266e244f04323c91ed52
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-12-06 02:05:13 +0300

    Task 3

--- Tyuiu.StoyakinNM.Sprint6.Task3.V9.Lib/DataService.cs
+++ Tyuiu.StoyakinNM.Sprint6.Task3.V9.Lib/DataService.cs
@@ -7,17 +7,14 @@ namespace Tyuiu.StoyakinNM.Sprint6.Task3.V9.Lib
         {
             int rows = matrix.GetLength(0);
 
-            // Извлекаем пятый столбец
             int[] fifthColumn = new int[rows];
             for (int i = 0; i < rows; i++)
             {
                 fifthColumn[i] = matrix[i, 4];
             }
 
-            // Сортируем пятый столбец по возрастанию
             Array.Sort(fifthColumn);
 
-            // Заменяем значения в пятом столбце отсортированными значениями
             for (int i = 0; i < rows; i++)
             {
                 matrix[i, 4] = fifthColumn[i];
